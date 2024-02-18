693659243f8af5a74fe923ea071c2b7a7fc0b809
devanshtiwari2002 <tiwaridevanshtech1@gmail.com>
2024-02-18 17:50:40 +0530

    java first program explained with use of comments

--- MyFirst.java
+++ MyFirst.java
@@ -1,8 +1,16 @@
-import java.lang.*;   // lang is a package 
+import java.lang.*; // lang is a package 
 
-class MyFirst{
-   public static void main(String arg[])
-    {
-       System.out.println("Hello world");
-    }
+class MyFirst { // class name should be same as file name
+   public static void main(String arg[]) { // main method
+      // main method should not return anything so void ,
+      // jvm should be able to see the main method so make public
+      // be can not use class unless we have cleared object , but if we want to create
+      // a class without object then it should be decleraed static
+      {
+         System.out.println("Hello world");
+      }
+   }
 }
+
+// MyFirst.class is a file after compile (byte coded)
+// jvm will call MyFirst.main()
